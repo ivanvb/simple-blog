55c635fb85f37857174e0e682218ebaa7a187c8e
ivanvb <ivanvalette@hotmail.com>
2021-05-09 16:52:42 -0400

    feat: add quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { GatsbyImage } from 'gatsby-plugin-image';
+
+const Quote = ({ backgroundImage, text, author }) => {
+    return (
+        <div className="relative h-64 md:h-104 w-full flex items-center justify-center">
+            <GatsbyImage
+                className="absolute h-full w-full"
+                image={backgroundImage}
+                objectFit="cover"
+                imgClassName="filter brightness-50"
+            />
+            <figure className="relative z-10 w-128 text-white container md:px-0">
+                <blockquote className="text-xl md:text-2xl mb-2 tracking-wide">{text}</blockquote>
+                <figcaption className="text-base md:text-lg">— {author}</figcaption>
+            </figure>
+        </div>
+    );
+};
+
+export default Quote;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import Quote from '../components/Quote';
 import '../styles/tailwind/build.css';
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+    const { contentfulHeroQuote: quoteData } = data;
     return (
         <div>
-            <p className="text-red-500">hello world</p>
+            <Quote
+                text={quoteData.text.text}
+                author={quoteData.author}
+                backgroundImage={quoteData.backgroundImage.gatsbyImageData}
+            />
         </div>
     );
 };
 
+export const query = graphql`
+    query {
+        contentfulHeroQuote {
+            text {
+                text
+            }
+            author
+            backgroundImage {
+                gatsbyImageData(width: 1140, placeholder: NONE)
+            }
+        }
+    }
+`;
 export default IndexPage;
