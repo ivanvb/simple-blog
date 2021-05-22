5e5ecdf164cb1e918dd111d2032cd4347e2fe9be
ivanvb <ivanvalette@hotmail.com>
2021-05-21 21:10:49 -0400

    hotfix: add missing default import to gatsby config and ssr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 const React = require('react');
-const Layout = require('./src/templates/Layout');
+const Layout = require('./src/templates/Layout').default;
 
 exports.wrapPageElement = ({ element, props }) => {
     return <Layout {...props}>{element}</Layout>;
