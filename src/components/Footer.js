9d279aaf5c6a710c8766241279f6c932a3d050d7
ivanvb <ivanvalette@hotmail.com>
2021-05-23 02:05:37 -0400

    feat: add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+    return <footer className="container pb-6 text-sm mt-auto">Copyright© 2021 - present</footer>;
+};
+
+export default Footer;
--- src/templates/Layout.js
+++ src/templates/Layout.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import '../styles/tailwind/build.css';
 
 const Layout = ({ children }) => {
-    return <main>{children}</main>;
+    return (
+        <>
+            <main>{children}</main>
+            <Footer />
+        </>
+    );
 };
 
 export default Layout;
