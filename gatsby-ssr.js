5ded5d189049b0424032e67d1fd10816eed79e71
ivanvb <ivanvalette@hotmail.com>
2021-05-28 21:04:30 -0400

    fix: initial theme load

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,12 @@
 const React = require('react');
 const Layout = require('./src/templates/Layout').default;
-const { setInitialColor, getInitialColorMode, DARK, LIGHT } = require('./src/util/theme');
+const {
+    setInitialColor,
+    getInitialColorMode,
+    toggleMode,
+    DARK,
+    LIGHT,
+} = require('./src/util/theme');
 
 const MagicScriptTag = () => {
     // Replaces every instance of the LIGHT variable with its literal value
@@ -14,8 +20,14 @@ const MagicScriptTag = () => {
         .replace(/LIGHT/g, `'${LIGHT}'`)
         .replace(/DARK/g, `'${DARK}'`);
 
+    const toggleFnc = toggleMode
+        .toString()
+        .replace(/LIGHT/g, `'${LIGHT}'`)
+        .replace(/DARK/g, `'${DARK}'`);
+
     // Instead of hardcoding the function as a string we take advantage of the Function.toString method
     const codeToRunOnClient = `
+    ${toggleFnc}
     ${initialColorFunction}
     (${colorFunction})()
   `;
--- src/util/theme.js
+++ src/util/theme.js
@@ -20,55 +20,51 @@ function getInitialColorMode() {
     return LIGHT;
 }
 
-function setInitialColor() {
-    const colorMode = getInitialColorMode();
-    const root = document.documentElement;
-    root.style.setProperty('--primary_background', colorMode === LIGHT ? 'white' : 'black');
-    root.style.setProperty('--primary_text', colorMode === LIGHT ? 'black' : 'white');
-    root.style.setProperty('--primary_accent', colorMode === LIGHT ? 'black' : 'white');
-    root.style.setProperty('--primary_secondary_accent', colorMode === LIGHT ? 'black' : 'white');
-}
-
-// Gets all the css variables defined in :root
-// Obtained from https://stackoverflow.com/a/56250392
-const cssVariables = [].slice
-    .call(document.styleSheets)
-    .map((styleSheet) => [].slice.call(styleSheet.cssRules))
-    .flat()
-    .filter((cssRule) => cssRule.selectorText === ':root')
-    .map((cssRule) => cssRule.cssText.split('{')[1].split('}')[0].trim().split(';'))
-    .flat()
-    .filter((text) => text !== '')
-    .map((text) => text.split(':'))
-    .map((parts) => ({ key: parts[0].trim(), value: parts[1].trim() }))
-    .reduce(
-        (acc, curr) => {
-            const id = curr.key.split('_')[0].replace(/--/, '');
-
-            if (id === LIGHT) {
-                acc[LIGHT].push(curr);
-            } else if (id === DARK) {
-                acc[DARK].push(curr);
-            } else {
-                acc.primary.push(curr);
-            }
-
-            return acc;
-        },
-        {
-            [LIGHT]: [],
-            [DARK]: [],
-            primary: [],
-        }
-    );
-
 function toggleMode(mode) {
+    // Gets all the css variables defined in :root
+    // Obtained from https://stackoverflow.com/a/56250392
+    const cssVariables = [].slice
+        .call(document.styleSheets)
+        .map((styleSheet) => [].slice.call(styleSheet.cssRules))
+        .flat()
+        .filter((cssRule) => cssRule.selectorText === ':root')
+        .map((cssRule) => cssRule.cssText.split('{')[1].split('}')[0].trim().split(';'))
+        .flat()
+        .filter((text) => text !== '')
+        .map((text) => text.split(':'))
+        .map((parts) => ({ key: parts[0].trim(), value: parts[1].trim() }))
+        .reduce(
+            (acc, curr) => {
+                const id = curr.key.split('_')[0].replace(/--/, '');
+
+                if (id === LIGHT) {
+                    acc[LIGHT].push(curr);
+                } else if (id === DARK) {
+                    acc[DARK].push(curr);
+                } else {
+                    acc.primary.push(curr);
+                }
+
+                return acc;
+            },
+            {
+                [LIGHT]: [],
+                [DARK]: [],
+                primary: [],
+            }
+        );
+
     if (mode && cssVariables[mode]) {
         cssVariables[mode].forEach(({ key, value }) => {
             document.documentElement.style.setProperty(key.replace(mode, 'primary'), value);
-            window.localStorage.setItem('color-mode', mode);
         });
+        window.localStorage.setItem('color-mode', mode);
     }
+}
+
+function setInitialColor() {
+    const colorMode = getInitialColorMode();
+    toggleMode(colorMode);
 }
 
 module.exports = { getInitialColorMode, toggleMode, setInitialColor, DARK, LIGHT };
