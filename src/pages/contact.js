c165cd29a5b1de6a9fee9cdbe11d3361d58bf88b
ivanvb <ivanvalette@hotmail.com>
2021-05-30 12:36:39 -0400

    feat: add form submit screen

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { H1, Input, Textarea } from '../components/base/index';
+import { Link } from 'gatsby';
+import { MdEmail } from 'react-icons/md';
 
 const ContactPage = () => {
+    const [isSubmitted, setSubmitted] = React.useState(false);
     async function handleSubmit(e) {
         e.preventDefault();
 
@@ -13,32 +16,53 @@ const ContactPage = () => {
 
             return acc;
         }, {});
+
+        console.log(formData);
+        setSubmitted(true);
     }
     return (
-        <div className="container pt-4 md:pt-12 text-center">
-            <H1>Contact Me</H1>
-            <p className="text-base md:text-lg">
-                Want to collaborate, ask something or suggest an idea for a future blogpost?{' '}
-                <br className="hidden md:block" />{' '}
-                <span className="font-bold">Send me a message!</span>
-            </p>
-            <form className="my-6 space-y-4 md:px-20" onSubmit={handleSubmit}>
-                <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-10">
-                    <Input name="firstname" id="firstname" label="First name" required />
-                    <Input name="lastname" id="lastname" label="Last name" required />
-                </div>
-                <div>
-                    <Input name="email" id="email" type="email" label="Email" required />
-                </div>
-                <div>
-                    <Textarea name="message" id="message" label="Message" required />
-                </div>
-                <div className="flex justify-start">
-                    <button className="w-full md:w-auto px-12 py-2 bg-primary-text text-primary-bg rounded-sm">
-                        Submit
-                    </button>
-                </div>
-            </form>
+        <div className="container pt-6 md:pt-12 text-center">
+            {isSubmitted ? (
+                <>
+                    <H1>Thanks for submitting!</H1>
+                    <p className="text-base md:text-lg">
+                        I will be getting in touch with you soon, but in the meantime you can
+                        <br className="hidden md:block" /> read some of my{' '}
+                        <Link to="/blog/all" className="underline">
+                            recent blog posts
+                        </Link>
+                    </p>
+                    <div className="bg-primary-text p-12 rounded-full text-primary-bg inline-block mt-0 md:mt-16 transform scale-75 md:scale-100">
+                        <MdEmail size={144} className="text-current" />
+                    </div>
+                </>
+            ) : (
+                <>
+                    <H1>Contact Me</H1>
+                    <p className="text-base md:text-lg">
+                        Want to collaborate, ask something or suggest an idea for a future blogpost?{' '}
+                        <br className="hidden md:block" />{' '}
+                        <span className="font-bold">Send me a message!</span>
+                    </p>
+                    <form className="my-6 space-y-4 md:px-20" onSubmit={handleSubmit}>
+                        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-10">
+                            <Input name="firstname" id="firstname" label="First name" required />
+                            <Input name="lastname" id="lastname" label="Last name" required />
+                        </div>
+                        <div>
+                            <Input name="email" id="email" type="email" label="Email" required />
+                        </div>
+                        <div>
+                            <Textarea name="message" id="message" label="Message" required />
+                        </div>
+                        <div className="flex justify-start">
+                            <button className="w-full md:w-auto px-12 py-2 bg-primary-text text-primary-bg rounded-sm">
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </>
+            )}
         </div>
     );
 };
